d6fda6f7189bb06a32c43b5cc3140a4e501b7427
jlaloi <julien.laloi@gmail.com>
2021-07-24 07:28:48 +0200

    clean up

--- src/server.tsx
+++ src/server.tsx
@@ -44,7 +44,6 @@ router
         <html lang="en">
         <head>
           <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <script type="module" src="/main.js"></script>
         </head>
         <body>
