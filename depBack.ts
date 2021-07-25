d2967c7edae30f7e4703733c122022f4fd610ab3
jlaloi <julien.laloi@gmail.com>
2021-07-25 09:40:31 +0200

    remove types

--- depBack.ts
+++ depBack.ts
@@ -4,7 +4,6 @@ import * as Colors from "https://deno.land/std@0.102.0/fmt/colors.ts";
 
 import { gzip } from "https://deno.land/x/denoflate@1.2.1/mod.ts";
 
-// @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from "https://esm.sh/react-dom@17.0.2/server";
 
 export { Application, Colors, gzip, ReactDOMServer, Router };
--- depFront.ts
+++ depFront.ts
@@ -1,6 +1,4 @@
-// @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
 import React from "https://esm.sh/react@17.0.2";
-// @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/react-dom.d.ts"
 import ReactDOM from "https://esm.sh/react-dom@17.0.2";
 
 export { React, ReactDOM };
