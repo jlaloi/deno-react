3cf694aeacc1b8667e0d9525ff558f1b43912747
jlaloi <julien.laloi@gmail.com>
2021-07-25 09:42:24 +0200

    fix exports

--- depBack.ts
+++ depBack.ts
@@ -1,9 +1,7 @@
-import { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
+export { gzip } from "https://deno.land/x/denoflate@1.2.1/mod.ts";
 
 import * as Colors from "https://deno.land/std@0.102.0/fmt/colors.ts";
-
-import { gzip } from "https://deno.land/x/denoflate@1.2.1/mod.ts";
-
 import ReactDOMServer from "https://esm.sh/react-dom@17.0.2/server";
 
-export { Application, Colors, gzip, ReactDOMServer, Router };
+export { Colors, ReactDOMServer };
